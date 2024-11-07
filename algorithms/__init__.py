e154f2cd758f35f1403235ed58fba0e68190f17d
Jacqueline Hong <jacquelinehong@Jacquelines-MacBook-Air.local>
2024-11-06 22:15:39 -0800

    Add initial repo skeleton

---
+++ algorithms/__init__.py
@@ -0,0 +1,28 @@
+# __init__.py
+# Importing each algorithm as part of the 'algorithms' package
+
+# TODO Algorithms:
+# from .linear_regression import LinearRegression
+# from .logistic_regression import LogisticRegression
+# from .decision_tree import DecisionTree
+# from .random_forest import RandomForest
+# from .k_means import KMeans
+from .knn import KNearestNeighbors
+# from .pca import PCA
+# from .gradient_descent import GradientDescent
+# from .naive_bayes import NaiveBayes
+# from .adaboost import AdaBoost
+
+__all__ = [
+    "KNearestNeighbors"
+    # "LinearRegression",
+    # "LogisticRegression",
+    # "DecisionTree",
+    # "RandomForest",
+    # "KMeans",
+    # "KNearestNeighbors",
+    # "PCA",
+    # "GradientDescent",
+    # "NaiveBayes",
+    # "AdaBoost"
+]
